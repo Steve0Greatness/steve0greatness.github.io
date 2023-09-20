b2c4b0167777910fc2698fb72c959779f7fb0750
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-09-19 19:02:55 -0700

    Start on routing for local_sv

--- DevHelp/local_server.py
+++ DevHelp/local_server.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, send_file as SendFile
+from os.path import exists as PathExists, join as JoinPath
+
+app = Flask(__name__)
+
+@app.route("/")
+def Index():
+    return SendFile("index.html")
+
+@app.route("/<path:path>")
+def Other(path):
+    FindFile = JoinPath("..", path).replace("\\", "/")
+    print(FindFile)
+    if PathExists(FindFile):
+        return SendFile(FindFile)
+    return SendFile("../404.html"), 404
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 9000, debug=True)
