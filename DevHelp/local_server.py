6b0e745fbfd5b3ab5db7a60f64c269b31eca9ba5
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-09-27 20:15:48 -0700

    Blog start

--- DevHelp/local_server.py
+++ DevHelp/local_server.py
@@ -1,17 +1,19 @@
 from flask import Flask, send_file as SendFile
-from os.path import exists as PathExists, join as JoinPath
+from os.path import exists as PathExists, join as JoinPath, isfile
+from os import getcwd
 
 app = Flask(__name__)
 
 @app.route("/")
 def Index():
-    return SendFile("index.html")
+    return SendFile("../index.html")
 
 @app.route("/<path:path>")
 def Other(path):
-    FindFile = JoinPath("..", path).replace("\\", "/")
-    print(FindFile)
-    if PathExists(FindFile):
+    FindFile = JoinPath(getcwd(), path).replace("\\", "/")
+    if not isfile(FindFile):
+        return SendFile(JoinPath(FindFile, "index.html").replace("\\", "/"))
+    elif PathExists(FindFile):
         return SendFile(FindFile)
     return SendFile("../404.html"), 404
 
