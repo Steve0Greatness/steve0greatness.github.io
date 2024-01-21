401bb0ab0e4b133ad85af295a407ec01dd071b15
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2024-01-21 15:28:41 -0800

    Add ability to build from dev server

--- dev-server.py
+++ dev-server.py
@@ -1,7 +1,16 @@
 from build import main as BuildStaticSite
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+
+@app.route("/")
+def Index():
+    return """<a href="/build">Rebuild site</a>"""
+
+@app.route("/build")
+def Builder():
+    BuildStaticSite()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 9000)
