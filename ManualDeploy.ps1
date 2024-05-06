bf9f94fbeaf7592df5be399e5a0346b658ba8b9f
Steve0Greatness <steve0greatnessiscool@gmail.com>
2024-05-05 22:49:20 -0700

    Manual deploy Bugfix + Force Push

--- ManualDeploy.ps1
+++ ManualDeploy.ps1
@@ -1,10 +1,10 @@
 python build.py cb-pages
 git switch --orphan pages
-ls | grep -v build | rm -r -f
+ls | grep -v build | rm -r -fo
 mv build/* .
 git add .
 git commit -m "Pages Build & Deploy"
-git push codeberg pages
-git push roundabout www
+git push codeberg pages -f
+git push roundabout www -f
 git branch -d pages
 git checkout main
